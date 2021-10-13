f606a94e28eb6ef4d082b72981848a8d5b20cb1e
Orlando <orlando.barba7980@alumnos.udg.mx>
2021-10-12 20:37:55 -0500

    Solution 1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Daily_Coding_Problem_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            Console.WriteLine(car(cons(a,b)));
+            Console.WriteLine(cdr(cons(a,b)));
+        }
+        static pair cons(int a, int b)
+        {
+            pair f;
+            return f = new pair(a, b);
+        }
+        static int car(pair f)
+        {
+            return f.GetFirstElement();
+        }
+        static int cdr(pair f)
+        {
+            return f.GetSecondElement();
+        }
+    }
+}
---
+++ pair.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daily_Coding_Problem_5
+{
+    class pair
+    {
+        int FirstElement;
+        int SecondElement;
+
+        public pair(int a,int b)
+        {
+            FirstElement = a;
+            SecondElement = b;
+        }
+        public int GetFirstElement()
+        {
+            return FirstElement;
+        }
+        public int GetSecondElement()
+        {
+            return SecondElement;
+        }
+    }
+}
